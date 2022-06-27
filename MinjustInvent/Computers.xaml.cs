c62c0aa3499c5a3bec22748aaa36613a9d33c4a3
David <david.chalyan25@gmail.com>
2022-06-27 14:17:37 +0300

    TODO: add computer table to DataGrid

--- MinjustInvent/Computers.xaml.cs
+++ MinjustInvent/Computers.xaml.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace MinjustInvent
 {
@@ -19,9 +12,14 @@ namespace MinjustInvent
     /// </summary>
     public partial class Computers : Window
     {
+        string connectionString;
+        SqlDataAdapter adapter;
+        DataTable computersTable;
+
         public Computers()
         {
             InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["minjustDB"].ConnectionString;
         }
 
         private void updateButton_Click(object sender, RoutedEventArgs e)
@@ -39,6 +37,43 @@ namespace MinjustInvent
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
             this.Close();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT * FROM Computers";
+            computersTable = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+
+                //установка команды на добавление для вызова хранимой процедуры
+                adapter.InsertCommand = new SqlCommand("sp_InsertComputer", connection);
+                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Name", SqlDbType.Text, 50, "Name"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Segment", SqlDbType.Text, 50, "Segment"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Ip", SqlDbType.Int, 50, "Ip"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@OperationSystem", SqlDbType.Text, 50, "OperationSystem"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Memory", SqlDbType.Int, 50, "Memory"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@InventNumber", SqlDbType.NVarChar, 50, "InventNumber"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@ComputerName", SqlDbType.Text, 50, "ComputerName"));
+
+                connection.Open();
+                adapter.Fill(computersTable);
+                computersGrid.ItemsSource = computersTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
         }
     }
 }
